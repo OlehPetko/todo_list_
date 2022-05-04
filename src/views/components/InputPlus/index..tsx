a867f5c05efe4c3a0b77742c3faa98482011f566
OLLO77 <petkooleg@gmail.com>
2022-05-04 12:18:44 +0100

    Initial commit

---
+++ src/views/components/InputPlus/index..tsx
@@ -0,0 +1,34 @@
+import React, {useCallback, useState} from "react";
+
+import styles from './index.module.scss'
+
+interface InputPlusProps {
+    onAdd: (title: string) => void
+}
+
+
+export const InputPlus: React.FC<InputPlusProps> = ({onAdd}) => {
+
+    const [inputValue, setInputValue] = useState('')
+
+    const addTask = useCallback(() => {
+        onAdd(inputValue)
+        setInputValue('')
+    }, [inputValue])
+
+    return (
+        <div className={styles.inputPlus}>
+            <input className={styles.inputPlusValue} value={inputValue}
+                   placeholder='type here....'
+                   onChange={event => setInputValue(event.target.value)}
+                   onKeyDown={(event) => {
+                       if (event.key === 'Enter') {
+                           addTask()
+                       }
+                   }}
+            />
+
+            <button onClick={addTask} aria-label='Add' className={styles.inputPlusButton}/>
+        </div>
+    )
+}
